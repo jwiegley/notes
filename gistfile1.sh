10c5387833bff4215cf2599321d46b110b1b727e
John Wiegley <johnw@newartisans.com>
2008-08-30 00:05:26 -0400

    Untabified.

--- gistfile1.sh
+++ gistfile1.sh
@@ -9,15 +9,15 @@ done
 
 for file in "$@"; do
     if [[ -L "$file" ]]; then
-	/bin/rm -f "$file"
+        /bin/rm -f "$file"
     elif [[ -e "$file" ]]; then
-	target=$(basename "$file")
-	index=$$
-	while [[ -e "$HOME/.Trash/$target" ]]; do
-	    target=$(basename "$file")-$index
-	    index=$((index + 1))
-	done
-	mv "$file" "$HOME/.Trash/$target" 2>&1
+        target=$(basename "$file")
+        index=$$
+        while [[ -e "$HOME/.Trash/$target" ]]; do
+            target=$(basename "$file")-$index
+            index=$((index + 1))
+        done
+        mv "$file" "$HOME/.Trash/$target" 2>&1
     fi
 done | grep -v "set owner/group.*Operation not permitted"
 

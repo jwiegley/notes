8752ba310c47d6bf21bf2c7b041e79cc1f9513c1
John Wiegley <johnw@newartisans.com>
2012-11-02 16:47:19 -0500

    Finished implementing context.hs

--- context.hs
+++ context.hs
@@ -2,9 +2,9 @@
 
 module Traversal where
 
+import Control.Applicative
 import Control.Comonad
 import Control.Comonad.Identity
-import Data.Functor
 import Data.Functor.Compose
 
 -- This is called:
@@ -18,55 +18,79 @@ import Data.Functor.Compose
 -- Costate, by Michael Lauer:
 --   http://mrlauer.wordpress.com/2010/12/18/rambling-thoughts-about-comonads/
 
-data Context c a = Context (c -> a) c
+data Context a b t = Context (b -> t) a
 
-instance Functor (Context c) where
+instance Functor (Context a b) where
   fmap f (Context g x) = Context (f . g) x
 
-instance Comonad (Context c) where
+instance a ~ b => Comonad (Context a b) where
   extract (Context g x)   = g x
   duplicate (Context g x) = Context (Context g) x
   extend f w              = fmap f (duplicate w)
 
-get :: Context c a -> c
+get :: Context a b t -> a
 get (Context _ x) = x
 
-modify :: (c -> c) -> Context c a -> a
+modify :: (a -> a) -> Context a a t -> t
 modify f (Context g x) = g (f x)
 
-experiment :: [c -> c] -> Context c a -> [a]
+experiment :: [a -> a] -> Context a a t -> [t]
 experiment fs w = map (`modify` w) fs
 
-liftCtx :: (a -> b) -> Context c a -> b
+liftCtx :: (t -> a) -> Context a a t -> a
 liftCtx f (Context g x) = f (g x)
 
 -- Functor-oriented version given by Edward Kmett as the basis for Lens (it is
 -- a "pre-applied lens", where Lens has the type (c -> f d) -> s -> f b)
 
-newtype ContextF c d b = ContextF {
-  runContextF :: forall f. Functor f => (c -> f d) -> f b
+newtype ContextF a b t = ContextF {
+  runContextF :: forall f. Functor f => (a -> f b) -> f t
 }
 
-instance Functor (ContextF c d) where
+instance Functor (ContextF a b) where
   fmap f (ContextF g) = ContextF (fmap f . g)
 
-instance c ~ d => Comonad (ContextF c d) where
+instance a ~ b => Comonad (ContextF a b) where
   extract (ContextF f)   = runIdentity (f Identity)
   duplicate (ContextF g) = getCompose (g (Compose . fmap ctxt . ctxt))
   extend f w             = fmap f (duplicate w)
 
+ctxt :: a -> ContextF a b b
 ctxt i = ContextF (\k -> k i)
 
-getF :: ContextF c d b -> c
-getF (ContextF g) = undefined
+getF :: ContextF a b t -> a
+getF (ContextF g) = getConst (g Const)
 
-modifyF :: (c -> c) -> ContextF c d b -> a
-modifyF f (ContextF g) = undefined
+modifyF :: (a -> a) -> ContextF a a t -> t
+modifyF f (ContextF g) = runIdentity (g (Identity . f))
 
-experimentF :: [c -> c] -> ContextF c d b -> [a]
-experimentF fs w = undefined
+experimentF :: [a -> a] -> ContextF a a t -> [t]
+experimentF fs w = map (`modifyF` w) fs
 
-liftCtxF :: (a -> b) -> ContextF c d b -> b
-liftCtxF f (ContextF g) = undefined
+liftCtxF :: (t -> a) -> ContextF a a t -> a
+liftCtxF f (ContextF g) = f (runIdentity (g Identity))
+
+ctxToCtxF :: Context a b t -> ContextF a b t
+ctxToCtxF (Context c x) = ContextF (\k -> fmap c (k x))
+
+ctxFToCtx :: ContextF a b t -> Context a b t
+ctxFToCtx w@(ContextF g) =  Context (g (flip const)) (getF w)
+
+main :: IO()
+main = do
+  let x = Context takeInts 3
+  print $ modify (+1) x
+
+  let y = ctxToCtxF x
+  print $ modifyF (+1) y
+
+  let z = ctxFToCtx y
+  print $ modify (+1) z
+
+  let w = ContextF (\k -> fmap takeInts (k 3))
+  print $ modifyF (+1) w
+
+  where
+    takeInts n = take n [1..10] :: [Int]
 
 -- context.hs ends here

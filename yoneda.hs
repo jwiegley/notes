1f5bae4d83d9d8eb7be420586c0c5dcf4ed28992
John Wiegley <johnw@newartisans.com>
2013-04-22 15:34:31 -0500

    Show why Yoneda is useful

--- yoneda.hs
+++ yoneda.hs
@@ -15,5 +15,7 @@ unembed :: Functor f => Yoneda f a -> f a
 unembed y = runYoneda y id
 
 main = do
-    let y = embed (Identity "Hello")
-    print $ runIdentity $ runYoneda y length
+    let y = \f -> runYoneda (embed (Identity "Hello")) f :: Identity Int
+        z = \f -> runYoneda (embed (Identity 10)) f      :: Identity Int
+    print $ runIdentity $ y length
+    print $ runIdentity $ z id

5786966062f3a1ca59ac341eec12e0f4f899471f
John Wiegley <johnw@newartisans.com>
2013-07-13 18:12:44 -0500

    changes

--- delcont.hs
+++ delcont.hs
@@ -2,25 +2,33 @@ module Main where
 
 import Control.Monad
 import Control.Monad.CC
+import Control.Monad.Free
 import Control.Monad.IO.Class
 
-data Stepper r m a = Step a (CCT r m (Stepper r m a)) | Done
+data Step r m a f  = Step a (CCT r m f)
+type Stepper r m a = Free (Step r m a) ()
 
+cata :: Functor f => (a -> b) -> (f b -> b) -> Free f a -> b
+cata z _ (Pure a) = z a
+cata z f (Free g) = f $ fmap (cata z f) g
+{-# INLINE cata #-}
+
+main :: IO ()
 main = runCCT $ do
-    Step v k <- reset $ \p -> do
-        forM_ [1,2,3,4,5] $ \val -> do
+    Free (Step v k) <- reset $ \p -> do
+        forM_ [1 :: Int,2,3,4,5] $ \val -> do
             liftIO $ putStrLn $ "val = " ++ show val
-            shift p (\k -> return $ Step val (k $ return ()))
-        return Done
+            shift p (\k -> return $ Free $ Step val (k $ return ()))
+        return (Pure ())
 
     liftIO $ print v
-    Step v k <- k
+    Free (Step v k) <- k
     liftIO $ print v
-    Step v k <- k
+    Free (Step v k) <- k
     liftIO $ print v
-    Step v k <- k
+    Free (Step v k) <- k
     liftIO $ print v
-    Step v k <- k
+    Free (Step v k) <- k
     liftIO $ print v
-    Done <- k
+    Pure () <- k
     return ()
---
+++ test.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import IdeSession
+import Data.Function
+
+main = do
+    sess <- initSession defaultSessionConfig
+                { configDir             = "."
+                , configGenerateModInfo = True
+                }
+
+    let cb = \_ -> return ()
+        update = flip (updateSession sess) cb
+
+    update $ updateCodeGeneration True
+    update $ updateStdoutBufferMode (RunLineBuffering Nothing)
+    update $ updateStderrBufferMode (RunLineBuffering Nothing)
+
+    update $ updateModule "A.hs"
+        "module A where\n\
+        \\n\
+        \main = putStrLn \"Hello!\"\n"
+
+    update $ updateModuleDelete "A.hs"
+    print =<< getSourceErrors sess
+
+  where
+    callRunWait ra = do
+        ebs <- runWait ra
+        case ebs of
+            Left bs -> putStrLn (show bs) >> callRunWait ra
+            Right rr -> print rr

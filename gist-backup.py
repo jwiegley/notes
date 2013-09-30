7ed266ec76dfa9cc38bd272ab577568583a94f7c
John Wiegley <johnw@newartisans.com>
2013-09-30 15:43:59 -0500

    Changes

--- gist-backup.py
+++ gist-backup.py
@@ -18,7 +18,7 @@ def download_gists(page):
     for gist in gists:
         call(['git', 
               'subtree', 
-              'pull' if isdir(gist['id']) else 'add',
+              'pull' if os.path.isdir(gist['id']) else 'add',
               '--prefix', 
               gist['id'], 
               gist['git_pull_url'], 

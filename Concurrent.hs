3d2b38316c29486c25c18a8e33dafb59e4fe58f8
John Wiegley <johnw@newartisans.com>
2013-10-09 14:34:14 -0500

    Added Concurrent.hs

---
+++ Concurrent.hs
@@ -0,0 +1,179 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Concurrent where
+
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.Async.Lifted
+import Control.Concurrent.STM
+import Control.Exception.Lifted
+import Control.Monad
+import Control.Monad.Base
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Free
+import Control.Monad.Trans.State
+import Data.Functor.Identity
+import Data.Tagged
+import Data.Traversable
+
+instance (MonadBase b m, Functor f) => MonadBase b (FreeT f m) where
+    liftBase = liftBaseDefault
+
+instance (MonadBaseControl b m, Functor f)
+         => MonadBaseControl b (FreeT f m) where
+    newtype StM (FreeT f m) a = StMFreeT (StM m (FreeF f a (FreeT f m a)))
+    liftBaseWith f =
+        FreeT $ liftM Pure $ liftBaseWith $ \runInBase -> f $ \k ->
+            liftM StMFreeT $ runInBase $ runFreeT k
+    restoreM (StMFreeT m) = FreeT . restoreM $ m
+
+newtype ConcurrentT m a = ConcurrentT { getConcurrentT :: FreeT Identity m a }
+
+runConcurrentT :: Monad m => ConcurrentT m a -> m a
+runConcurrentT (ConcurrentT m) = iterT runIdentity m
+
+instance Monad m => Functor (ConcurrentT m) where
+    fmap f (ConcurrentT m) = ConcurrentT (fmap f m)
+
+instance Monad m => Monad (ConcurrentT m) where
+    return = ConcurrentT . FreeT . return . Pure
+    ConcurrentT (FreeT m) >>= k = ConcurrentT . FreeT $ do
+        a <- m -- serialize actions in the Monad
+        case a of
+            Pure a' -> runFreeT . getConcurrentT $ k a'
+            Free r  -> return . Free $ fmap (>>= getConcurrentT . k) r
+
+instance MonadBaseControl IO m => Applicative (ConcurrentT m) where
+    pure = return
+    ConcurrentT f <*> ConcurrentT a =
+        -- run actions concurrently in the Applicative
+        ConcurrentT $ do
+            (f', a') <- concurrently f a
+            return $ f' a'
+
+instance MonadTrans ConcurrentT where
+    lift = ConcurrentT . lift
+
+instance MonadIO m => MonadIO (ConcurrentT m) where
+    liftIO = ConcurrentT . liftIO
+
+instance (MonadBaseControl IO m, MonadBase IO m)
+         => MonadBase IO (ConcurrentT m) where
+    liftBase = liftBaseDefault
+
+instance MonadBaseControl IO m => MonadBaseControl IO (ConcurrentT m) where
+    newtype StM (ConcurrentT m) a = StMConcurrentT (StM (FreeT Identity m) a)
+    liftBaseWith f =
+        ConcurrentT $ liftBaseWith $ \runInBase -> f $ \k ->
+            liftM StMConcurrentT $ runInBase $ getConcurrentT k
+    restoreM (StMConcurrentT m) = ConcurrentT . restoreM $ m
+
+newtype ConcurrentPoolT s m a = ConcurrentPoolT
+    { getConcurrentPoolT :: ConcurrentT (StateT (TVar (Tagged s Int)) m) a }
+
+runConcurrentPoolT :: MonadIO m
+                   => Int -> (forall s. ConcurrentPoolT s m a) -> m a
+runConcurrentPoolT count (ConcurrentPoolT m) = do
+    counter <- liftIO $ newTVarIO (Tagged count)
+    evalStateT (runConcurrentT m) counter
+
+instance Monad m => Functor (ConcurrentPoolT s m) where
+    fmap f (ConcurrentPoolT m) = ConcurrentPoolT (fmap f m)
+
+instance Monad m => Monad (ConcurrentPoolT s m) where
+    return = ConcurrentPoolT . return
+    ConcurrentPoolT m >>= k = ConcurrentPoolT $ m >>= getConcurrentPoolT . k
+
+instance (MonadBaseControl IO m, MonadIO m)
+         => Applicative (ConcurrentPoolT s m) where
+    pure = return
+    ConcurrentPoolT f <*> ConcurrentPoolT a = ConcurrentPoolT $ do
+        counter <- lift get
+        bracket
+            (liftIO $ atomically $ do
+                  count <- readTVar counter
+                  writeTVar counter (pred count)
+                  return count)
+            (const $ liftIO $ atomically $ do
+                  count <- readTVar counter
+                  writeTVar counter (succ count))
+            $ \count ->
+                if count > 0
+                then f <*> a -- execute asynchronously, there are slots free
+                else do      -- execute synchronously, no slots at the moment
+                    f' <- f
+                    a' <- a
+                    return $ f' a'
+
+instance MonadTrans (ConcurrentPoolT s) where
+    lift = ConcurrentPoolT . lift . lift
+
+instance MonadIO m => MonadIO (ConcurrentPoolT s m) where
+    liftIO = ConcurrentPoolT . liftIO
+
+instance (MonadBaseControl IO m, MonadBase IO m, MonadIO m)
+         => MonadBase IO (ConcurrentPoolT s m) where
+    liftBase = liftBaseDefault
+
+instance (MonadBaseControl IO m, MonadIO m)
+         => MonadBaseControl IO (ConcurrentPoolT s m) where
+    newtype StM (ConcurrentPoolT s m) a =
+        StMConcurrentPoolT
+            (StM (ConcurrentT (StateT (TVar (Tagged s Int)) m)) a)
+    liftBaseWith f =
+        ConcurrentPoolT $ liftBaseWith $ \runInBase -> f $ \k ->
+            liftM StMConcurrentPoolT $ runInBase $ getConcurrentPoolT k
+    restoreM (StMConcurrentPoolT m) = ConcurrentPoolT . restoreM $ m
+
+main :: IO ()
+main = do
+    putStrLn "Using the Monad instance, things happen serially"
+    runConcurrentT $ do
+        delay "start 1" (return ()) "end 1"
+        delay "start 2" (return ()) "end 2"
+
+    putStrLn "Using the Applicative instance, things happen concurrently"
+    print =<<
+        runConcurrentT
+            (sequenceA
+                [ delay "scompute 1" (return (1 :: Int)) "ecompute 1"
+                , delay "scompute 2" (return (2 :: Int)) "ecompute 2"
+                , delay "scompute 3" (return (3 :: Int)) "ecompute 3"
+                , delay "scompute 4" (return (4 :: Int)) "ecompute 4"
+                , delay "scompute 5" (return (5 :: Int)) "ecompute 5"
+                , delay "scompute 6" (return (6 :: Int)) "ecompute 6"
+                , delay "scompute 7" (return (7 :: Int)) "ecompute 7"
+                , delay "scompute 8" (return (8 :: Int)) "ecompute 8"
+                , delay "scompute 9" (return (9 :: Int)) "ecompute 9"
+                ])
+
+    putStrLn "Using a pool restricts the number of concurrent computations"
+    print =<<
+        runConcurrentPoolT 5
+            (sequenceA
+                [ delay "scompute 1" (return (1 :: Int)) "ecompute 1"
+                , delay "scompute 2" (return (2 :: Int)) "ecompute 2"
+                , delay "scompute 3" (return (3 :: Int)) "ecompute 3"
+                , delay "scompute 4" (return (4 :: Int)) "ecompute 4"
+                , delay "scompute 5" (return (5 :: Int)) "ecompute 5"
+                , delay "scompute 6" (return (6 :: Int)) "ecompute 6"
+                , delay "scompute 7" (return (7 :: Int)) "ecompute 7"
+                , delay "scompute 8" (return (8 :: Int)) "ecompute 8"
+                , delay "scompute 9" (return (9 :: Int)) "ecompute 9"
+                ])
+  where
+    delay :: MonadIO m => String -> m a -> String -> m a
+    delay before f after = do
+        liftIO $ putStrLn before
+        liftIO $ threadDelay 2000000
+        x <- f
+        liftIO $ putStrLn after
+        return x

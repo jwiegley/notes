35fff6c3812b91e95dfc189789258b36cb71a7c7
John Wiegley <johnw@newartisans.com>
2014-09-29 11:19:04 -0500

    Added Exists.v

---
+++ Exists.v
@@ -0,0 +1,12 @@
+Lemma marvimias : forall t : Set -> Prop,
+  (exists x, t x) <-> (forall y : Prop, (forall x, t x -> y) -> y).
+Proof.
+  split; intros.
+    destruct H.
+    apply (H0 x).
+    apply H.
+  apply H.
+  intros.
+  exists x.
+  apply H0.
+Qed.

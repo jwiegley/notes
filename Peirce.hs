6ae5b18ee2bd0a55d9f30bb104823437826d2560
John Wiegley <johnw@newartisans.com>
2015-04-06 20:36:46 -0500

    Complete Peirce.hs

--- Peirce.hs
+++ Peirce.hs
@@ -6,14 +6,13 @@ module Peirce where
 
 import Data.Void
 
-type Not a = a -> Void
-
+type Not a  = a -> Void
 type Pierce = forall a b. ((a -> b) -> a) -> a
-type LEM = forall a. Either (Not a) a
+type LEM    = forall a.   Either (Not a) a
 
 callCC_lem :: Pierce -> LEM
-callCC_lem callCC = let l@(Right x) = Right (callCC (\f -> f x)) in l
+callCC_lem callCC = callCC $ \k -> Left (k . Right)
 
 lem_callCC :: LEM -> Pierce
 lem_callCC (Left n)  = \k -> k (absurd . n)
-lem_callCC (Right p) = \k -> p
+lem_callCC (Right p) = \_ -> p

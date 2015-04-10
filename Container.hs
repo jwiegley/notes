0dbee09ba434891e5acd455a3a031d184a41658c
John Wiegley <johnw@newartisans.com>
2015-04-09 23:10:53 -0500

    More thoughts in Container.hs

--- Container.hs
+++ Container.hs
@@ -1,32 +1,51 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Container where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Control
-import Control.Monad.Trans.State
-import Data.Maybe
-import Data.Monoid
+import Data.Void
+import GHC.TypeLits
 
-data Container (p :: k -> *) (a :: *) where
-    -- Need pi-types for this!
-    Box :: forall p (x :: k) a. x -> (p x -> a) -> Container p a
+-- In a dependently typed language, a container's accessor function's input
+-- argument type is determined by its shape; in Haskell, we must use a GADT to
+-- indicate each family of shapes by different constructors, so we can fix the
+-- argument type using a type parameter.
+--
+-- What we lose is the common type 'Container P a'. Instead, our common
+-- interface is Functor, which is mostly all that can be done with containers
+-- in general.
 
-type family S k :: * where
-    S () = Float
+data Fin :: Nat -> * where
+    F1 :: Fin 1
+    FS :: Fin n -> Fin (succ n)
 
-main :: IO ()
-main = do
-    let action = do
-            x <- get
-            return $ floor x + 10
-    case Box (evalState action) :: Container S Int of
-        Box b -> print (b (10.0 :: Float))
+-- List Container:
+--   Shape    : Set         := Nat
+--   Input    : Shape -> Set := forall n : nat, Fin n
+--   shape    : Shape
+--   accessor : Input shape -> a
+--     Empty >> Input shape = Void, where shape = 0
+--     Cons  >> Input shape = Fin (succ shape), where shape = succ shape
+data ListS :: Nat -> * -> * -> * where
+    Empty :: (Void         -> a) -> ListS 0        Void           a
+    Cons  :: (Fin (succ n) -> a) -> ListS (succ n) (Fin (succ n)) a
+
+-- However, these cannot be constructed for any list!
+
+instance Functor (ListS s p) where
+    fmap f (Empty x) = Empty (fmap f x)
+    fmap f (Cons x)  = Cons (fmap f x)
+
+-- State Container:
+--   Shape    : Set         := ()
+--   Input    : Shape -> Set := fun _ => s
+--   shape    : Shape
+--   accessor : Input shape -> a
+--     HasState >> Input shape = s, where shape = ()
+data StateS :: () -> * -> * -> * where
+    HasState :: (s -> a) -> StateS '() s a
+
+instance Functor (StateS s p) where
+    fmap f (HasState x) = HasState (fmap f x)

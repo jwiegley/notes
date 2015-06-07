110a6c3f77bf24471b1e6b58eb605e03dab9da26
John Wiegley <johnw@newartisans.com>
2015-06-07 00:43:26 -0500

    Complete proofs in DFeuer.v

--- DFeuer.v
+++ DFeuer.v
@@ -1,39 +1,37 @@
-Require Import Hask.Prelude.
-Require Import Hask.Data.List.
+Require Import Ssreflect.ssreflect.
+Require Import Ssreflect.ssrfun.
 
-Lemma dfeuer : forall (A B : Type) (g : B -> A) (f : A -> B),
-  injective f -> cancel g f -> bijective f.
+(* For any injective function f, if there exists a right-sided inverse g,
+   g is a two-sided inverse for f. *)
+
+Theorem dfeuer : forall (A B : Type) (g : B -> A) (f : A -> B),
+  injective f -> cancel g f -> cancel f g.
+Proof. move=> *; exact: inj_can_sym. Qed.
+
+(* Not every injective function has a left-sided inverse. *)
+
+Theorem arkeet :
+  ~ (forall A B (f : A -> B), injective f -> exists g, cancel f g).
 Proof.
-  move=> A B g f Hinj Heqe.
-  pose H := inj_can_sym Heqe Hinj.
-  exact: Bijective H Heqe.
+  move=> H.
+  have Ha : exists A B (f : A -> B) (Hinj : injective f),
+              forall (g : B -> A), ~ cancel f g.
+    exists False.
+    exists unit.
+    exists (fun (_ : False) => tt).
+    eexists.
+      rewrite /injective.
+      move=> x1 x2 Heqe.
+      contradiction.
+    move=> H0 g.
+    apply H0.
+    exact tt.
+  case: Ha => [? [? [? [Hinj H0]]]].
+  move: (H _ _ _ Hinj) => [g ?].
+  contradiction (H0 g).
 Qed.
 
-Lemma arkeet : forall (A : finType) (B : eqType) (f : A -> B),
-  size (enum A) > 0 -> injective f -> exists g, cancel f g.
-Proof.
-  move=> A B f Hsz Hinj.
-  have H := mem_image Hinj.
-  specialize (H (fun x => x \in enum A)).
-  pose g := (fun (x : B) =>
-    let s := [seq (f x, x) | x <- enum A] in
-    snd (nth (x, safe_hd (enum A) Hsz) s
-             (find (fun z => x == fst z) s))).
-  exists g.
-  move=> x.
-  elim: (enum A) => //= [y ys IHys] in H Hsz g *.
-  case: ys => [|z zs] /= in IHys H Hsz g *.
-    clear IHys.
-    rewrite /g /=.
-    case X: (x == y) in H.
-      move/eqP in X.
-      by rewrite X eq_refl.
-    rewrite inj_eq // X /=.
-    admit.
-  rewrite /g /=.
-  case X: (x == y) in H.
-    move/eqP in X.
-    by rewrite X eq_refl.
-  rewrite inj_eq // X /=.
-  admit.
-Abort.
+(* Every function with a left-sided inverse is injective. *)
+
+Theorem johnw : forall A B (f : A -> B) (g : B -> A), cancel f g -> injective f.
+Proof. move=> *; exact: can_inj. Qed.

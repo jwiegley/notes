d0425475b324d797ab2a9923970f7db36b1d65b3
John Wiegley <johnw@newartisans.com>
2015-06-12 02:19:56 -0500

    Remove some impossible code

--- PipesSimple.hs
+++ PipesSimple.hs
@@ -2,14 +2,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module PipesSimple where
 
 import Control.Applicative
-import Control.Monad
 import Control.Monad.Trans.Cont
-import Data.Functor.Identity
 import Data.Profunctor
 
 -- The current Proxy type
@@ -90,60 +87,3 @@ data ReqRes a' a b' b r
 newtype (f :.: g) a = C (f (g a))
 
 type Proxy6 a' a b' b m r = Free (m :.: ReqRes a' a b' b) r
-
--- -- We can again Boehm-Berarducci encode and apply Cont. What we see is that
--- -- the Free monad teases out the 'm' effects, and drops Cont from the result
--- -- value.
-
--- newtype ReqRes2 a' a b' b r = ReqRes2 {
---     runReqRes2 :: (a' -> Cont r a) -> (b -> Cont r b') -> r
--- }
-
--- mapContT' :: (r -> s) -> ContT r m a -> ContT s m a
--- mapContT' f m = ContT $ \(k :: a -> m s) -> liftM f $ runContT m k
-
--- instance Functor (ReqRes2 a' a b' b) where
---     fmap f (ReqRes2 p) = ReqRes2 $ \g h ->
---         p (mapContT' f . g) h
-
--- type Proxy7 a' a b' b m r = Free (m :.: ReqRes2 a' a b' b) r
-
--- toProxy7 :: forall a' a b' b m r. Monad m
---          => Proxy a' a b' b m r -> Proxy7 a' a b' b m r
--- toProxy7 (Request a' fa) = F $ C $ return $ ReqRes2 $ \req res ->
---     flip runCont id $ req a' >>= \a  -> ContT $ \k -> k (toProxy7 (fa  a))
--- toProxy7 (Respond b fb') = F $ C $ return $ ReqRes2 $ \req res ->
---     flip runCont id $ res b  >>= \b' -> ContT $ \k -> k (toProxy7 (fb' b'))
--- toProxy7 (M m) = F $ C $ liftM (\x -> ReqRes2 $ \req res -> toProxy7 x) m
--- toProxy7 (Pure r) = P r
-
--- fromProxy7 :: Monad m => Proxy7 a' a b' b m r -> Proxy a' a b' b m r
--- fromProxy7 (P p) = Pure p
--- fromProxy7 (F (C p)) = M $ do
---     ReqRes2 g <- liftM (fmap fromProxy7) p
---     let p' = g (\a' -> ContT $ \k -> Identity $ Request a' (runIdentity . k))
---                (\b  -> ContT $ \k -> Identity $ Respond b  (runIdentity . k))
---     return p'
-
--- -- Now instead of a Kleisli arrow from a pair of Kleisli arrows to a
--- -- result, we have simpler Kleisli arrows (Cont vs. ContT) with structure
--- -- around them:
-
--- newtype CA2 a b s = CA2 { runCA2 :: a -> Cont s b }
-
--- newtype (f :*: g) a = R (f a, g a)
--- newtype (f :!: g) a = B (f (g a) a)
-
--- type Proxy8 a' a b' b m r =
---     Free (m :.: ((->) :!: (CA2 a' a :*: CA2 b' b'))) r
-
--- toProxy8 :: forall a' a b' b m r. Monad m
---          => Proxy a' a b' b m r -> Proxy8 a' a b' b m r
--- toProxy8 (Request a' fa) = F $ C go
---   where
---     go :: (m :.: ((->) :!: (CA2 a' a :*: CA2 b' b'))) r
---     go = pure $ B $ \(R (req, res)) ->
---         runCA2 req a' >>= \a -> toProxy8 (fa  a)
--- toProxy8 (Respond b fb') = undefined
--- toProxy8 (M m) = undefined
--- toProxy8 (Pure r) = P r

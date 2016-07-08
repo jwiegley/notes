232ceae17c51d903e13b4012ff36e5a479902402
John Wiegley <johnw@newartisans.com>
2016-07-08 14:56:12 -0700

    Update EmacsLisp.v

--- EmacsLisp.v
+++ EmacsLisp.v
@@ -8,143 +8,145 @@ Open Scope string_scope.
 Open Scope list_scope.
 
 Section Lisp.
-  Variable var : Type.
+  Inductive type : Type :=
+    | T : type
+    | Nil : type
+    | Integer : type
+    | Char : type
+    | String : type
+    | Cons : type -> type -> type
+    | Func : type -> type -> type
+    | Symbol : type
+    | Error : type.
 
-  Inductive Expr :=
-    | EVar : var -> Expr
-    | Enil : Expr
-    | Et : Expr
-    | EInteger : nat -> Expr
-    (* | EFloat : nat -> Expr *)
-    | EChar : ascii -> Expr
-    | EString : string -> Expr
-    (* | EVector : nat -> Expr *)
-    (* | EHashTable : nat -> Expr *)
-    | ESubr : (var -> Expr) -> Expr
-    | EError : string -> Expr
-    | ESymbol : string -> Expr
-    | EQuote : Expr -> Expr
-    | ECons : Expr -> Expr -> Expr.
+  Inductive truth := TT.
+  Inductive falsehood := NT.
+  Record symbol := genSym { symbolId : string }.
+  Record error := genError { errorMsg : string }.
+
+  Variable var : type -> Type.
+
+  Inductive term : type -> Type :=
+    | TVar : forall t, var t -> term t
+    | Tnil : term Nil
+    | Tt : term T
+    | TInteger : nat -> term Integer
+    (* | TFloat : nat -> term *)
+    | TChar : ascii -> term Char
+    | TString : string -> term String
+    (* | TVector : nat -> term *)
+    (* | THashTable : nat -> term *)
+    | TAbs : forall dom cod, (var dom -> term cod) -> term (Func dom cod)
+    | TApp : forall dom cod, term (Func dom cod) -> term dom -> term cod
+    | TError : string -> term Error
+    | TSymbol : string -> term Symbol
+    | TCons : forall a b, term a -> term b -> term (Cons a b).
 End Lisp.
 
-Arguments EVar [var] _.
-Arguments Enil [var].
-Arguments Et [var].
-Arguments EInteger [var] _.
-Arguments EChar [var] _.
-Arguments EString [var] _.
-Arguments ESubr [var] _.
-Arguments EError [var] _.
-Arguments ESymbol [var] _.
-Arguments EQuote [var] _.
-Arguments ECons [var] _ _.
+Arguments TVar [var t] _.
+Arguments Tnil [var].
+Arguments Tt [var].
+Arguments TInteger [var] _.
+Arguments TChar [var] _.
+Arguments TString [var] _.
+Arguments TAbs [var dom cod] _.
+Arguments TApp [var dom cod] _ _.
+Arguments TError [var] _.
+Arguments TSymbol [var] _.
+Arguments TCons [var _ _] _ _.
 
-Definition Term := forall var, Expr var.
+Definition Term t := forall var, term var t.
 
-Notation "([ x ^ y ])" := (fun var => @ECons var x y).
-Notation "# x" := (fun var => @ESymbol var x) (at level 100).
+Notation "([ x ^ y ])" := (fun var => @TCons var x y).
+Notation "# x" := (fun var => @TSymbol var x) (at level 100).
 
-Definition nil : Term := fun var => @Enil var.
+Definition nil : Term Nil := fun var => @Tnil var.
 
-Notation "([ f ])" := (fun var => @ECons var f nil).
+Notation "([ f ])" := (fun var => @TCons var f nil).
 Notation "([ f , .. , x ])" :=
-  (fun var => @ECons var (f var) .. (@ECons var (x var) (nil var)) ..)
+  (fun var => @TCons var (f var) .. (@TCons var (x var) (nil var)) ..)
   (right associativity).
 
-Definition str (s : string) := fun var => @EString var s.
-Definition num (n : nat)    := fun var => @EInteger var n.
-Definition quote' (x : Term) := fun var => @EQuote var (x var).
+Definition str (s : string) := fun var => @TString var s.
+Definition num (n : nat)    := fun var => @TInteger var n.
 
-Definition Flength var (e : Expr var) : Expr var :=
-  let fix go e :=
-    match e with
-    | EVar x     => EVar x
-    | Enil       => EInteger 0
-    | Et         => EError "wrong-type-argument"
-    | EInteger x => EError "wrong-type-argument"
-    | EChar x    => EError "wrong-type-argument"
-    | EString x  => EError "wrong-type-argument"
-    | ESubr x    => EError "wrong-type-argument"
-    | EError x   => EError x
-    | ESymbol _  => EError "wrong-type-argument"
-    | EQuote _   => EError "wrong-type-argument"
-    | ECons a d  =>
-      match go d with
-      | EInteger n => EInteger (S n)
-      | x => x
-      end
-    end in
-  go e.
-
-Compute Flength nat (([ num 10 , num 20 , num 30 ]) nat).
-
-Fixpoint squash {var} (e : Expr (Expr var)) : Expr var :=
-  let fix go e :=
-    match e with
-    | EVar x     => x
-    | Enil       => Enil
-    | Et         => Et
-    | EInteger x => EInteger x
-    | EChar x    => EChar x
-    | EString x  => EString x
-    | ESubr f    => ESubr (fun x => squash (f (EVar x)))
-    | EError x   => EError x
-    | ESymbol _  => EError "wrong-type-argument"
-    | EQuote x   => EQuote (squash x)
-    | ECons a d  => ECons (squash a) (squash d)
-    end in
-  go e.
-
-Definition subr (f : forall var, Expr var -> Expr var) :=
-  fun var => @ESubr var (fun v => f var (EVar v)).
-
-Check ([ #"message" , str "Hello, world!" , ([ subr Flength , num 10 ]) ]).
-
-Fixpoint ident {var} (e : Expr var) : Expr var :=
+Fixpoint Flength {var t} (e : term var t) : term var Error + term var Integer :=
   match e with
-  | EVar x     => EVar x
-  | Enil       => Enil
-  | Et         => Et
-  | EInteger x => EInteger x
-  | EChar x    => EChar x
-  | EString x  => EString x
-  | ESubr k    => ESubr (fun x => ident (k x))
-  | EError x   => EError x
-  | ESymbol s  => ESymbol s
-  | EQuote x   => EQuote (ident x)
-  | ECons a d  => ECons (ident a) (ident d)
+  | TVar _     => inl (TError "wrong-type-argument")
+  | Tnil       => inr (TInteger 0)
+  | Tt         => inl (TError "wrong-type-argument")
+  | TInteger _ => inl (TError "wrong-type-argument")
+  | TChar _    => inl (TError "wrong-type-argument")
+  | TString _  => inl (TError "wrong-type-argument")
+  | TAbs _     => inl (TError "wrong-type-argument")
+  | TApp _ _   => inl (TError "wrong-type-argument")
+  | TError x   => inl (TError x)
+  | TSymbol _  => inl (TError "wrong-type-argument")
+  | TCons _ d  => match Flength d with
+                  | inr (TInteger n) => inr (TInteger (S n))
+                  | _ => inl (TError "wrong-type-argument")
+                  end
   end.
 
-Fixpoint eval (e : Term) : Term := fun var =>
-  let fix go (e : Expr var) :=
-    match e with
-    | EVar x     => EVar x
-    | Enil       => Enil
-    | Et         => Et
-    | EInteger x => EInteger x
-    | EChar x    => EChar x
-    | EString x  => EString x
-    | ESubr k    => ESubr (fun v : var => go (k v))
-    | EError x   => EError x
-    | ESymbol s  => ESymbol s      (* need to lookup variable value *)
-    | EQuote x   => x
-    | ECons (ESubr f) x => go (f (go x))
-    | ECons a d  => ECons (go a) (go d)
-    end in
-  go (e var).
+Fixpoint squash {var t} (e : term (term var) t) : term var t :=
+  match e with
+  | TVar x     => x
+  | Tnil       => Tnil
+  | Tt         => Tt
+  | TInteger x => TInteger x
+  | TChar x    => TChar x
+  | TString x  => TString x
+  | TAbs f     => TAbs (fun x => squash (f (TVar x)))
+  | TApp e1 e2 => TApp (squash e1) (squash e2)
+  | TError x   => TError x
+  | TSymbol x  => TSymbol x
+  | TCons a d  => TCons (squash a) (squash d)
+  end.
 
-(* (length . ((quote . ((1 . (2 . (3 . nil))))) . nil)) *)
+(* Definition subr (f : forall var, term var -> term var) := *)
+(*   fun var => @ESubr var (fun v => f var (EVar v)). *)
 
-Eval compute [subr quote'] in
-    (([ subr (@Flength) ,
-        quote' ([ num 10 , num 20 , num 30 ]) ]) nat).
+(* Check ([ #"message" , str "Hello, world!" , ([ subr Flength , num 10 ]) ]). *)
 
-Example ex_length_3 :
-  eval (([ subr (@Flength) ,
-           quote' ([ num 10 , num 20 , num 30 ]) ]) (Expr nat)) =
-  num 3 nat.
-Proof.
-  unfold subr, squash.
-  simpl.
-  unfold num.
-  reflexivity. Qed.
+Fixpoint ident {var t} (e : term var t) : term var t :=
+  match e with
+  | TVar x     => TVar x
+  | Tnil       => Tnil
+  | Tt         => Tt
+  | TInteger x => TInteger x
+  | TChar x    => TChar x
+  | TString x  => TString x
+  | TAbs f     => TAbs (fun x => ident (f x))
+  | TApp e1 e2 => TApp (ident e1) (ident e2)
+  | TError x   => TError x
+  | TSymbol s  => TSymbol s
+  | TCons a d  => TCons (ident a) (ident d)
+  end.
+
+Fixpoint typeDenote (t : type) : Type :=
+  match t with
+  | T => truth
+  | Nil => falsehood
+  | Integer => nat
+  | Char => ascii
+  | String => string
+  | Cons a d => (typeDenote a * typeDenote d)%type
+  | Func arg ret => typeDenote arg -> typeDenote ret
+  | Symbol => symbol
+  | Error => error
+  end.
+
+Fixpoint termDenote {t} (e : term typeDenote t) : typeDenote t :=
+  match e with
+  | TVar x     => x
+  | Tnil       => NT
+  | Tt         => TT
+  | TInteger x => x
+  | TChar x    => x
+  | TString x  => x
+  | TAbs f     => fun v => termDenote (f v)
+  | TApp e1 e2 => termDenote e1 (termDenote e2)
+  | TError x   => genError x
+  | TSymbol s  => genSym s
+  | TCons a d  => (termDenote a, termDenote d)
+  end.

4c363ad843d23fb99a109da634ff73fdf3491efd
John Wiegley <johnw@newartisans.com>
2016-10-26 11:49:12 -0700

    Update Fib.hs

--- Fib.hs
+++ Fib.hs
@@ -1,42 +1,28 @@
 module Main where
 
 import Control.Monad
-import Data.Maybe
 import Data.Time.Clock
 
-fib :: Int -> IO Int
-fib = fmap (fromMaybe 0) . go 0
-  where
-    go acc 0 = return $ Just acc
-    go acc 1 = go (acc+1) 0
-    go acc n = do
-        t <- go acc (n-2)
-        case t of
-            Nothing -> return Nothing
-            Just n' -> do
-                t' <- go acc (n-1)
-                return $ case t' of
-                    Nothing  -> Nothing
-                    Just n'' -> Just (n' + n'')
+foldl :: Monad m => (a -> Maybe ()) -> (b -> a -> b) -> b -> [a] -> m (Maybe b)
+foldl _ _ z []     = return (Just z)
+foldl b f z (x:xs) =
+    fmap join $ forM (b x) $ \() ->
+        Main.foldl b f (f z x) xs
 
-fibSlow :: Int -> IO Int
-fibSlow = fmap (fromMaybe 0) . go 0
-  where
-    go acc 0 = return $ Just acc
-    go acc 1 = go (acc+1) 0
-    go acc n = do
-        t <- go acc (n-2)
-        fmap join $ forM t $ \n' -> do
-            t' <- go acc (n-1)
-            forM t' $ \n'' ->
-                return (n' + n'')
+foldl2 :: Monad m => (a -> Maybe ()) -> (b -> a -> b) -> b -> [a] -> m (Maybe b)
+foldl2 _ _ z []     = return (Just z)
+foldl2 b f z (x:xs) =
+    case b x of
+        Nothing -> return Nothing
+        Just () -> Main.foldl2 b f (f z x) xs
 
 main :: IO ()
-main = test fib >> test fibSlow
+main = test Main.foldl >> test Main.foldl2
   where
     test f = do
         t0 <- getCurrentTime
-        n <- f 30
+        n <- f (\x -> if x<10000000 then Just () else Nothing) (+)
+                 (0 :: Int) ([1..100000000] :: [Int])
         print n
         t1 <- getCurrentTime
         print $ diffUTCTime t1 t0

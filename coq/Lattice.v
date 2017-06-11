a8442154c654756eba283873a527499ac2862d89
John Wiegley <johnw@newartisans.com>
2017-06-10 17:23:29 -0700

    More work on lattices; very close now

--- coq/Lattice.v
+++ coq/Lattice.v
@@ -6,6 +6,7 @@ Require Import Coq.Lists.List.
 Require Import Coq.Relations.Relations.
 Require Import Coq.Classes.RelationClasses.
 Require Import Coq.quote.Quote.
+Require Import Coq.Wellfounded.Lexicographic_Product.
 
 Generalizable All Variables.
 
@@ -40,43 +41,50 @@ Class Order (A : Set) := {
 
 Infix "≤" := ord (at level 50).
 
-Class LOSet (A : Set) := {
-  order :> Order A;
-  lattice :> Lattice A;
-  consistent: forall a b, a ≤ b <-> a = a ⊓ b
+Class LOSet {A : Set} `(@Order A) `(@Lattice A) := {
+  meet_consistent : forall a b, a ≤ b <-> a = a ⊓ b;
+  join_consistent : forall a b, a ≤ b <-> b = a ⊔ b
 }.
-
-Coercion is_lattice {A : Set} (O : LOSet A) : Lattice A := lattice.
 
 Section Lattice.
 
-Context `{l : LOSet A}.
+Context `{O : Order A}.
+Context `{L : Lattice A}.
+Context `{@LOSet A O L}.
 
 Theorem meet_is_glb : forall a b : A,
   forall x, x ≤ a /\ x ≤ b <-> x ≤ a ⊓ b.
 Proof.
   split; intros.
     intuition.
-    apply consistent in H0.
-    apply consistent in H1.
-    apply consistent.
-    rewrite <- meet_associative, <- H0.
+    apply meet_consistent in H1.
+    apply meet_consistent in H2.
+    apply meet_consistent.
+    rewrite <- meet_associative, <- H1.
     assumption.
-  apply consistent in H.
-  rewrite H; clear H.
-  split; apply consistent.
+  apply meet_consistent in H0.
+  rewrite H0; clear H0.
+  split; apply meet_consistent.
     rewrite meet_associative.
-    f_equal.
-    rewrite meet_associative.
-    rewrite (meet_commutative b a).
-    rewrite <- meet_associative.
+    rewrite (meet_commutative (a ⊓ b) a).
+    rewrite <- (meet_associative a).
     rewrite meet_idempotent.
     reflexivity.
   rewrite meet_associative.
-  f_equal.
   rewrite meet_associative.
   rewrite meet_idempotent.
   reflexivity.
+Qed.
+
+Theorem meet_prime : forall a b : A,
+  forall x, a ≤ x \/ b ≤ x -> a ⊓ b ≤ x.
+Proof.
+  intros.
+  destruct H0;
+  apply meet_consistent in H0;
+  apply meet_consistent; [rewrite meet_commutative|];
+  rewrite meet_associative;
+  rewrite <- H0; reflexivity.
 Qed.
 
 Theorem join_is_lub : forall a b : A,
@@ -84,10 +92,35 @@ Theorem join_is_lub : forall a b : A,
 Proof.
   split; intros.
     intuition.
-    apply consistent in H0.
-    apply consistent in H1.
-    apply consistent.
-Admitted.
+    apply join_consistent in H1.
+    apply join_consistent in H2.
+    apply join_consistent.
+    rewrite join_associative, <- H2.
+    assumption.
+  apply join_consistent in H0.
+  rewrite H0; clear H0.
+  split; apply join_consistent.
+    rewrite <- join_associative.
+    rewrite <- join_associative.
+    rewrite join_idempotent.
+    reflexivity.
+  rewrite (join_commutative a b).
+  rewrite <- join_associative.
+  rewrite <- join_associative.
+  rewrite join_idempotent.
+  reflexivity.
+Qed.
+
+Theorem join_prime : forall a b : A,
+  forall x, x ≤ a \/ x ≤ b -> x ≤ a ⊔ b.
+Proof.
+  intros.
+  destruct H0;
+  apply join_consistent in H0;
+  apply join_consistent; [|rewrite join_commutative];
+  rewrite <- join_associative;
+  rewrite <- H0; reflexivity.
+Qed.
 
 Set Decidable Equality Schemes.
 
@@ -101,7 +134,7 @@ Proof.
   induction x;
   unfold not; intros;
   try discriminate.
-  inversion H; subst.
+  inversion H0; subst.
   contradiction.
 Qed.
 
@@ -110,7 +143,7 @@ Proof.
   induction y;
   unfold not; intros;
   try discriminate.
-  inversion H; subst.
+  inversion H0; subst.
   contradiction.
 Qed.
 
@@ -119,7 +152,7 @@ Proof.
   induction x;
   unfold not; intros;
   try discriminate.
-  inversion H; subst.
+  inversion H0; subst.
   contradiction.
 Qed.
 
@@ -128,7 +161,7 @@ Proof.
   induction y;
   unfold not; intros;
   try discriminate.
-  inversion H; subst.
+  inversion H0; subst.
   contradiction.
 Qed.
 
@@ -148,111 +181,85 @@ Fixpoint depth (t : Term) : nat :=
 
 Inductive Subterm : Term -> Term -> Prop :=
   | Meet1 : forall t1 t2, Subterm t1 (Meet t1 t2)
-  | Meet2 : forall t1 t2, t1 <> t2 -> Subterm t2 (Meet t1 t2)
+  | Meet2 : forall t1 t2, Subterm t2 (Meet t1 t2)
   | Join1 : forall t1 t2, Subterm t1 (Join t1 t2)
-  | Join2 : forall t1 t2, t1 <> t2 -> Subterm t2 (Join t1 t2).
+  | Join2 : forall t1 t2, Subterm t2 (Join t1 t2).
 
 Definition Subterm_inv_t : forall x y, Subterm x y -> Prop.
 Proof.
   intros [] [] f;
   match goal with
-  | [ H : Subterm ?X (Meet ?X ?Z) |- Prop ] =>
-    exact (f = Meet1 _ _)
-  | [ H : Subterm ?X (Join ?X ?Z) |- Prop ] =>
-    exact (f = Join1 _ _)
   | [ H : Subterm ?X (Meet ?Y ?Z) |- Prop ] =>
-    destruct (Term_eq_dec Y X) as [e|e]; subst;
-    [ exact (f = Meet1 _ _)
+    destruct (Term_eq_dec X Y); subst;
+    [ destruct (Term_eq_dec X Z); subst;
+      [ exact (f = Meet1 _ _ \/ f = Meet2 _ _)
+      | exact (f = Meet1 _ _) ]
     | destruct (Term_eq_dec X Z); subst;
-      [ exact (f = Meet2 Y X e)
+      [ exact (f = Meet2 _ _)
       | exact False ] ]
   | [ H : Subterm ?X (Join ?Y ?Z) |- Prop ] =>
-    destruct (Term_eq_dec Y X) as [e|e]; subst;
-    [ exact (f = Join1 _ _)
+    destruct (Term_eq_dec X Y); subst;
+    [ destruct (Term_eq_dec X Z); subst;
+      [ exact (f = Join1 _ _ \/ f = Join2 _ _)
+      | exact (f = Join1 _ _) ]
     | destruct (Term_eq_dec X Z); subst;
-      [ exact (f = Join2 Y X e)
+      [ exact (f = Join2 _ _)
       | exact False ] ]
   | _ => exact False
   end.
 Defined.
-
-Theorem UIP_dec_not : forall (A : Type),
-  (forall x y:A, {x = y} + {x <> y}) ->
-  forall (x y:A) (p1 p2:x <> y), p1 = p2.
-Proof.
-  intros.
-  unfold not in *.
-  (* jww (2017-06-10): Is there any way to avoid an axiom here? *)
-  Require Import FunctionalExtensionality.
-  extensionality p.
-  subst; contradiction.
-Qed.
-
-Print Assumptions UIP_dec_not.
 
 Corollary Subterm_inv x y f : Subterm_inv_t x y f.
 Proof.
   pose proof Term_eq_dec.
   destruct f, t1, t2; simpl;
   repeat destruct (Term_eq_dec _ _); subst;
-  unfold eq_rect_r;
   try (rewrite e || rewrite <- e);
+  try (rewrite e0 || rewrite <- e0);
   try congruence;
-  try rewrite <- Eqdep_dec.eq_rect_eq_dec; eauto;
-  f_equal;
-  apply UIP_dec_not; auto.
+  try rewrite <- Eqdep_dec.eq_rect_eq_dec; eauto; simpl; intuition;
+  try rewrite <- Eqdep_dec.eq_rect_eq_dec; eauto; simpl; intuition.
 Qed.
 
-Program Instance Subterm_StrictOrder : StrictOrder Subterm.
+Program Instance Subterm_Irreflexive : Irreflexive Subterm.
 Next Obligation.
   repeat intro.
-  pose proof (Subterm_inv _ _ H).
-  inversion H; subst; simpl in *.
+  pose proof (Subterm_inv _ _ H0).
+  inversion H0; subst; simpl in *.
   - now apply (Meet_acc_l x t2).
   - now apply (Meet_acc_r t1 x).
   - now apply (Join_acc_l x t2).
   - now apply (Join_acc_r t1 x).
 Qed.
-Next Obligation.
-  repeat intro.
-  pose proof (Subterm_inv _ _ H).
-  pose proof (Subterm_inv _ _ H0).
-  inversion H; subst; simpl in *;
-  inversion H0; subst; simpl in *.
-  (* jww (2017-06-10): At least the way that Subterm is currently defined, it
-     is not transitive. *)
-Admitted.
 
 Lemma Subterm_wf : well_founded Subterm.
 Proof.
   constructor; intros.
-  inversion H; subst; simpl in *.
-  - induction y, t2; simpl in *;
-    constructor; intros;
-    inversion H0; subst; clear H0;
-    try (apply IHy1; constructor);
-    try (apply IHy2; constructor).
-  - induction y, t1; simpl in *;
-    constructor; intros;
-    inversion H1; subst; clear H1.
-    destruct y1.
-    destruct (Term_eq_dec (Var n) (Var n0)).
-    apply IHy1.
-    constructor.
-    try (apply IHy1; constructor);
-    try (apply IHy2; constructor).
+  inversion H0; subst; simpl in *;
+  induction y;
+  induction t1 || induction t2;
+  simpl in *;
+  constructor; intros;
+  inversion H1; subst; clear H1;
+  try (apply IHy1; constructor);
+  try (apply IHy2; constructor).
+Qed.
 
 Reserved Notation "〚 t 〛 env" (at level 9).
 
 Fixpoint eval (t : Term) (env : nat -> A) : A :=
   match t with
   | Var n => env n
-  | Meet t1 t2 =>〚t1〛env ⊓〚t2〛env
-  | Join t1 t2 =>〚t1〛env ⊔〚t2〛env
+  | Meet t1 t2 => 〚t1〛env ⊓ 〚t2〛env
+  | Join t1 t2 => 〚t1〛env ⊔ 〚t2〛env
   end where "〚 t 〛 env" := (eval t env).
 
 Definition Leq   (s t : Term) : Prop := forall env, 〚s〛env ≤ 〚t〛env.
+Arguments Leq _ _ /.
+
+(* Note that Equiv can be computed from Leq. *)
 Definition Equiv (s t : Term) : Prop := forall env, 〚s〛env = 〚t〛env.
+Arguments Equiv _ _ /.
 
 Reserved Infix "≲" (at level 30).
 
@@ -261,61 +268,130 @@ Arguments R /.
 
 Open Scope lazy_bool_scope.
 
+Ltac meets_and_joins leq :=
+  repeat destruct (leq (_, _) _);
+  simpl in *;
+  subst;
+  repeat match goal with
+  | [ H : (_, _) = (_, _) |- _ ] => progress (inversion H; subst)
+  | [ H : bool |- _ ] => destruct H
+  end;
+  try discriminate;
+  simpl in *;
+  repeat match goal with
+  | [ |- _ ⊔ _ ≤ _ ] => apply join_is_lub; split; firstorder idtac
+  | [ |- _ ≤ _ ⊔ _ ] => apply join_prime; firstorder idtac
+  | [ |- _ ≤ _ ⊓ _ ] => apply meet_is_glb; split; firstorder idtac
+  | [ |- _ ⊓ _ ≤ _ ] => apply meet_prime; firstorder idtac
+  end.
+
+Local Obligation Tactic :=
+  program_simpl; try (abstract (constructor; constructor)).
+
+Program Fixpoint leqb (p : Term * Term) {wf R p} : bool :=
+  match p with
+  (* 1. If s = Var i and t = Var j, then s ≲ t holds iff i = j. *)
+  | (Var i, Var j) => Nat.eqb i j
+
+  (* 2. If s = Join s1 s2, then s ≲ t holds iff s1 ≲ t and s2 ≲ t. *)
+  | (Join s1 s2, t) => leqb (s1, t) &&& leqb (s2, t)
+
+  (* 3. If t = Meet t1 t2, then s ≲ t holds iff s ≲ t1 and s ≲ t2. *)
+  | (s, Meet t1 t2) => leqb (s, t1) &&& leqb (s, t2)
+
+  (* 4. If s = Var i and t = Join t1 t2, then s ≲ t holds iff s ≲ t1 or s ≲ t2. *)
+  | (Var i, Join t1 t2) => leqb (Var i, t1) ||| leqb (Var i, t2)
+
+  (* 5. If s = Meet s1 s2 and t = Var i, then s ≲ t holds iff s1 ≲ t or s2 ≲ t. *)
+  | (Meet s1 s2, Var i) => leqb (s1, Var i) ||| leqb (s2, Var i)
+
+  (* 6. If s = Meet s1 s2 and t = Join t1 t2, then s ≲ t holds iff s1 ≲ t or
+        s2 ≲ t or s ≲ t1 or s ≲ t2. *)
+  | (Meet s1 s2, Join t1 t2) =>
+    leqb (s1, Join t1 t2) ||| leqb (s2, Join t1 t2) |||
+    leqb (Meet s1 s2, t1) ||| leqb (Meet s1 s2, t2)
+  end.
+Next Obligation.
+  apply wf_symprod;
+  apply Subterm_wf.
+Qed.
+
+(* Whitman's decision procedure. *)
 Program Fixpoint leq (p : Term * Term) {wf R p} :
   { b : bool | b = true -> Leq (fst p) (snd p) } :=
   match p with
-  | (s, t) =>
-    let leqb x y := proj1_sig (leq (x, y)) in
-    match (s, t) with
-    | (Var i, Var j)           => nat_eq_bool i j
-    | (Join s1 s2, t)          => leqb s1 t &&& leqb s2 t
-    | (s, Meet t1 t2)          => leqb s t1 &&& leqb s t2
-    | (Var m, Join t1 t2)      => leqb s t1 ||| leqb s t2
-    | (Meet s1 s2, Var n)      => leqb s1 t ||| leqb s2 t
-    | (Meet s1 s2, Join t1 t2) => leqb s1 t ||| leqb s2 t ||| leqb s t1 ||| leqb s t2
-    end
-  end where "s ≲ t" := (leq (s, t)).
+  (* 1. If s = Var i and t = Var j, then s ≲ t holds iff i = j. *)
+  | (Var i, Var j) => nat_eq_bool i j
+
+  (* 2. If s = Join s1 s2, then s ≲ t holds iff s1 ≲ t and s2 ≲ t. *)
+  | (Join s1 s2, t) =>
+    exist _ (proj1_sig (leq (s1, t)) &&& proj1_sig (leq (s2, t))) _
+
+  (* 3. If t = Meet t1 t2, then s ≲ t holds iff s ≲ t1 and s ≲ t2. *)
+  | (s, Meet t1 t2) =>
+    exist _ (proj1_sig (leq (s, t1)) &&& proj1_sig (leq (s, t2))) _
+
+  (* 4. If s = Var i and t = Join t1 t2, then s ≲ t holds iff s ≲ t1 or s ≲ t2. *)
+  | (Var i, Join t1 t2) =>
+    exist _ (proj1_sig (leq (Var i, t1)) ||| proj1_sig (leq (Var i, t2))) _
+
+  (* 5. If s = Meet s1 s2 and t = Var i, then s ≲ t holds iff s1 ≲ t or s2 ≲ t. *)
+  | (Meet s1 s2, Var i) =>
+    exist _ (proj1_sig (leq (s1, Var i)) ||| proj1_sig (leq (s2, Var i))) _
+
+  (* 6. If s = Meet s1 s2 and t = Join t1 t2, then s ≲ t holds iff s1 ≲ t or
+        s2 ≲ t or s ≲ t1 or s ≲ t2. *)
+  | (Meet s1 s2, Join t1 t2) =>
+    exist _ (proj1_sig (leq (s1, Join t1 t2)) |||
+             proj1_sig (leq (s2, Join t1 t2)) |||
+             proj1_sig (leq (Meet s1 s2, t1)) |||
+             proj1_sig (leq (Meet s1 s2, t2))) _
+  end.
 Next Obligation.
-  destruct (leq (x, t1)).
-    constructor.
-    admit.
-  simpl in *.
-  unfold Leq in l0.
-   apply left_sym.
-Admitted.
+  destruct (nat_eq_bool i j); simpl in *; subst.
+  rewrite y; reflexivity.
+Qed.
+Next Obligation. meets_and_joins leq. Qed.
+Next Obligation. meets_and_joins leq. Qed.
+Next Obligation. meets_and_joins leq. Qed.
+Next Obligation. meets_and_joins leq. Qed.
 Next Obligation.
-  unfold Leq; intros; simpl.
-  destruct (nat_eq_bool i j) eqn:Heqe; simpl in H; subst.
-  rewrite y.
-  reflexivity.
+  destruct (leq (s1, Join t1 t2)) as [[] o1]; simpl in *.
+    intros; simpl in *.
+    apply meet_prime; left; apply o1; reflexivity.
+  destruct (leq (s2, Join t1 t2)) as [[] o2]; simpl in *.
+    intros; simpl in *;
+    apply meet_prime; right; apply o2; reflexivity.
+  destruct (leq (Meet s1 s2, t1)) as [[] o3]; simpl in *.
+    intros; meets_and_joins leq.
+  destruct (leq (Meet s1 s2, t2)) as [[] o4]; simpl in *.
+    intros; meets_and_joins leq.
+  discriminate.
 Qed.
 Next Obligation.
-  unfold Leq; intros; simpl.
-  inversion Heq_p; subst.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-  unfold MR, R; simpl.
-Admitted.
+  apply wf_symprod;
+  apply Subterm_wf.
+Qed.
 
-Notation "s ≲ t" := (leq (s, t)).
+Notation "s ≲ t" := (leq (s, t)) (at level 30).
+
+Definition leq_correct {t u : Term} (Heq : ` (t ≲ u) = true) :
+  forall env, 〚t〛env ≤ 〚u〛env := proj2_sig (leq (t, u)) Heq.
+
+Corollary let_proj1_sig {B : Type} (P : B -> Prop) (e : {x : B | P x}) :
+  (let (x, _) := e in x) = proj1_sig e.
+Proof. reflexivity. Qed.
+
+Definition leqb_correct (p : Term * Term) : proj1_sig (leq p) = leqb p.
+Proof.
+  destruct p as [[] []]; simpl.
+  unfold leqb; simpl.
+Abort.
+
+End Lattice.
+
+Notation "〚 t 〛 env" := (@eval _ _ t env) (at level 9).
+Notation "s ≲ t" := (@leq _ _ _ _ (s, t)) (at level 30).
 
 Import ListNotations.
 
@@ -333,14 +409,14 @@ Ltac addToList x xs :=
   | false => constr:((x, xs))
   end.
 
-Ltac allVars xs m j e :=
+Ltac allVars xs e :=
   match e with
-  | m ?e1 ?e2 =>
-    let xs := allVars xs m j e1 in
-    allVars xs m j e2
-  | j ?e1 ?e2 =>
-    let xs := allVars xs m j e1 in
-    allVars xs m j e2
+  | ?e1 ⊓ ?e2 =>
+    let xs := allVars xs e1 in
+    allVars xs e2
+  | ?e1 ⊔ ?e2 =>
+    let xs := allVars xs e1 in
+    allVars xs e2
   | _ => addToList e xs
   end.
 
@@ -352,27 +428,20 @@ Ltac lookup x xs :=
     constr:(S n)
   end.
 
-Ltac reifyTerm env m j t :=
+Ltac reifyTerm env t :=
   match t with
-  | m ?X1 ?X2 =>
-    let r1 := reifyTerm env m j X1 in
-    let r2 := reifyTerm env m j X2 in
+  | ?X1 ⊓ ?X2 =>
+    let r1 := reifyTerm env X1 in
+    let r2 := reifyTerm env X2 in
     constr:(Meet r1 r2)
-  | j ?X1 ?X2 =>
-    let r1 := reifyTerm env m j X1 in
-    let r2 := reifyTerm env m j X2 in
+  | ?X1 ⊔ ?X2 =>
+    let r1 := reifyTerm env X1 in
+    let r2 := reifyTerm env X2 in
     constr:(Join r1 r2)
   | ?X =>
     let n := lookup X env in
     constr:(Var n)
   end.
-
-Lemma leq_correct : forall t u : Term,
-  Is_true (` (t ≲ u)) -> forall env, 〚t〛env ≤ 〚u〛env.
-Proof.
-  intros.
-  induction t, u; simpl in *.
-Admitted.
 
 Ltac functionalize xs :=
   let rec loop n xs' :=
@@ -384,30 +453,33 @@ Ltac functionalize xs :=
     end in
   loop 0 xs.
 
-Ltac reify m j :=
+Ltac reify :=
   match goal with
   | [ |- ?S ≤ ?T ] =>
-    let xs  := allVars tt m j S in
-    let xs' := allVars xs m j T in
-    let r1  := reifyTerm xs' m j S in
-    let r2  := reifyTerm xs' m j T in
+    let xs  := allVars tt S in
+    let xs' := allVars xs T in
+    let r1  := reifyTerm xs' S in
+    let r2  := reifyTerm xs' T in
     let env := functionalize xs' in
-    pose xs';
-    pose env;
-    pose r1;
-    pose r2;
-    change (〚r1〛env ≤ 〚r2〛env);
-    apply leq_correct;
-    vm_compute
+    (* pose xs'; *)
+    (* pose env; *)
+    (* pose r1; *)
+    (* pose r2; *)
+    change (〚r1〛env ≤ 〚r2〛env)
   end.
 
-Ltac lattice := reify meet join.
+Ltac lattice := reify; apply leq_correct; vm_compute.
 
-Lemma median_inequality : forall x y z : A,
+Lemma sample_inequality `{LOSet A} : forall a b : A,
+  a ⊓ b ≤ a ⊔ b.
+Proof.
+  intros.
+  lattice.
+Abort.
+
+Lemma median_inequality `{LOSet A} : forall x y z : A,
   (x ⊓ y) ⊔ (y ⊓ z) ⊔ (z ⊓ x) ≤ (x ⊔ y) ⊓ (y ⊔ z) ⊓ (z ⊔ x).
 Proof.
   intros.
   lattice.
-Admitted.
-
-End Lattice.
+Abort.

d217d581de2a395c4d850365d590498f88699aa0
John Wiegley <johnw@newartisans.com>
2017-09-10 13:48:12 +0100

    Many improvements; adiM is now done

--- haskell/Darais.hs
+++ haskell/Darais.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Darais where
 
+import Control.Monad
+import Data.Functor.Identity
 import Debug.Trace
 
 newtype Fix f = Fix { unFix :: f (Fix f) }
@@ -15,17 +16,37 @@ data ExprF r = Val Int | Add r r
 
 type Expr = Fix ExprF
 
-cata' :: Monoid b
-      => (ExprF a -> a)
-      -> ((Expr -> (b, Maybe a)) -> (Expr -> (b, Maybe a)))
-      -> (Expr -> (b, Maybe a))
-cata' f g = g (fmap (fmap f . sequenceA) . traverse (cata' f g) . unFix)
-
-eval :: Expr -> ((), Maybe Int)
-eval = cata' phi psi
+adi :: (Monoid b, Applicative s, Traversable t)
+    => (t a -> a)
+    -> ((Fix t -> (b, s a)) -> Fix t -> (b, s a))
+    -> Fix t -> (b, s a)
+adi f g = g (go . traverse (adi f g) . unFix)
   where
-    phi (Val x) = x
-    phi (Add x y) = x + y
+    go = fmap (fmap f . sequenceA)
 
+adiM :: (Monoid b, Applicative s, Traversable s, Traversable t, Monad m)
+     => (t a -> m a)
+     -> ((Fix t -> m (b, s a)) -> Fix t -> m (b, s a))
+     -> Fix t -> m (b, s a)
+adiM f g = g ((go <=< traverse (adiM f g)) . unFix)
+  where
+    go = traverse (traverse f . sequenceA) . sequenceA
+
+-- A type application is needed below because we do not generate any value
+-- from the analysis; it does tracing only.
+
+denote :: ExprF Int -> Int
+denote (Val x)   = x
+denote (Add x y) = x + y
+
+eval :: Expr -> Int
+eval = runIdentity . snd . adi @() denote psi
+  where
     psi k v@(Fix (Val _))   = trace "Evaluating Val" $ k v
     psi k v@(Fix (Add _ _)) = trace "Evaluating Add" $ k v
+
+evalM :: Expr -> IO Int
+evalM = fmap (runIdentity . snd) . adiM @() (pure <$> denote) psi
+  where
+    psi k v@(Fix (Val _))   = putStrLn "Evaluating Val" >> k v
+    psi k v@(Fix (Add _ _)) = putStrLn "Evaluating Add" >> k v

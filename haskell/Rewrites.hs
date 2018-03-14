c9898c74657ad5942975ca4311920d690680e212
John Wiegley <johnw@newartisans.com>
2018-03-14 15:30:54 -0700

    Fix a typo

--- haskell/Rewrites.hs
+++ haskell/Rewrites.hs
@@ -19,13 +19,13 @@ zippers xs = go []
 --   a sequence of subsitutions that will transform the LHS into the RHS? The
 --   return value is a list of indices of rewrites applied.
 --
---   For every substitution allowed, we only consider the reverse direction.
+--   For every substitution allowed, we also consider the reverse direction.
 --
 -- >>> findSubstitutions [([2,3],[5,3,5]),
 --                        ([5,3],[9]),
 --                        ([5,3],[3,5])]
 --                        ([1,2,3,4,5],[1,9,5,4,5])
--- Just [0,2]
+-- Just [0,1]
 findSubstitutions :: Eq a => [([a], [a])] -> ([a],[a]) -> Maybe [Int]
 findSubstitutions hyps (have, want) =
     case go [] [] have of

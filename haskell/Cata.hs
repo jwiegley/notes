a82ba91a134b83733d0fab0a3b1db00a1ac851a6
John Wiegley <johnw@newartisans.com>
2018-04-27 11:07:37 -0700

    Update Cata.hs

--- haskell/Cata.hs
+++ haskell/Cata.hs
@@ -17,7 +17,7 @@ algComp :: Functor f => (f a -> a) -> (f b -> b) -> f (a, b) -> (a, b)
 algComp f g x = (f (fmap fst x), g (fmap snd x))
 
 instance Functor f => Applicative (Cata f) where
-    pure x = Cata undefined (const x)
+    pure x = Cata (const ()) (const x)
     Cata falg fred <*> Cata galg gred =
         Cata (algComp falg galg) (\(f, g) -> fred f (gred g))
 
@@ -28,17 +28,57 @@ data ListF a r = Nil | Cons a r deriving Functor
 
 type List a = Fix (ListF a)
 
+-- This is just an F-Algebra -> Church encoder
+listFold :: ListF a r -> (a -> r -> r) -> r -> r
+listFold l step beg = phi l
+  where
+    phi = \case Nil -> beg; Cons x y -> step x y
+
 sumList :: Num a => Alg (ListF a) a
 sumList = \case Nil -> 0; Cons x y -> x + y
 
+sumList' :: forall a. Num a => Alg (ListF a) a
+sumList' l = listFold l (+) 0
+
 lenList :: Alg (ListF a) Int
 lenList = \case Nil -> 0; Cons _ r -> 1 + r
+
+lenList' :: Alg (ListF a) Int
+lenList' l = listFold l (const (1+)) 1
 
 liftWithRed :: Alg f a -> (a -> b) -> Cata f b
 liftWithRed = Cata
 
 lift :: Alg f a -> Cata f a
 lift = flip liftWithRed id
+
+-- This type appears in the 'foldl' library on Hackage:
+--   https://hackage.haskell.org/package/foldl-1.4.0/docs/src/Control.Foldl.html#Fold
+data Fold a b = forall x. Fold (x -> a -> x) x (x -> b)
+
+-- It's easy enough to apply to our fixed point, if we know its Church
+-- encoding. Note that we can auto-generate these Church encoded
+-- representations using Template Haskell, using my 'recursors' library:
+--   https://hackage.haskell.org/package/recursors
+foldList :: Fold a b -> List a -> b
+foldList (Fold step beg red) = red . cata (\l -> listFold l (flip step) beg)
+
+data NatF r = O | S r deriving Functor
+
+type Nat = Fix NatF
+
+-- If we don't know the Church encoding, we can still use a Fold by just
+-- applying 'cata' as we normally would.
+foldNat :: Fold () a -> Nat -> a
+foldNat (Fold step beg red) = red . cata (\case O -> beg; S r -> step r ())
+
+-- However, while this allows us to use composed Folds on known F-algebras, it
+-- doesn't let us compose algorithms for any F-algebra the way Cata does.
+
+-- In a sense, this is due to the same distinction as between Church and Scott
+-- encodings: the Fold type has lost the knowledge of 'f' at each level of the
+-- recursion, making selective decision making based on that information
+-- impossible, whereas Cata depends on it.
 
 main :: IO ()
 main = do
@@ -48,4 +88,5 @@ main = do
                               (Fix Nil)))))) :: List Int
 
     print $ runCata ((,) <$> lift sumList <*> lift lenList) xs
+
     -- => (60,3) in a single pass over the list

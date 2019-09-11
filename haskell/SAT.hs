ec2b846b9ed8bf141eb410a645ac58ed2094abf7
John Wiegley <john@dfinity.org>
2019-09-10 21:29:43 -0700

    changes

--- haskell/SAT.hs
+++ haskell/SAT.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE GADTs #-}
+
 module SAT where
+
+import Control.Applicative
+import Test.QuickCheck
+
+prop_distr :: (Applicative f, Alternative f,
+              Arbitrary a, Arbitrary b,
+              Eq (f b), f ~ [], a ~ Int, b ~ Int)
+           => f a -> f a -> Bool
+prop_distr x y = (f <*> (x <|> y)) == ((f <*> x) <|> (f <*> y))
+  where
+    f = [(+10), (+20), (+30), (+40)]
+
+prop_distr' :: (Applicative f, Alternative f,
+               Arbitrary a, Arbitrary b,
+               Eq (f b), f ~ [], a ~ Int, b ~ Int)
+            => f a -> Bool
+prop_distr' x = ((f <|> g) <*> x) == ((f <*> x) <|> (g <*> x))
+  where
+    f = [(+10), (+20), (+30), (+40)]
+    g = [(+50), (+60), (+70), (+80)]
 
 rel :: Eq b => (a -> b) -> a -> b -> Bool
 rel f x y = f x == y

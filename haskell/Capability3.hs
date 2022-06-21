a4e39cc89e6f60d80d52d832ab7ac210bceb1a63
John Wiegley <johnw@newartisans.com>
2022-06-21 13:18:09 -0700

    Updates to Capability3.hs

--- haskell/Capability3.hs
+++ haskell/Capability3.hs
@@ -100,14 +100,14 @@ lookupSignature :: forall s effs. MonadCapability s effs
 lookupSignature name arg val = do
   defs <- ask @ModuleDefs
   case M.lookup name (capabilitySigs defs) of
-      Nothing -> throwError $ UnknownCapability name
-      Just sig ->
-        if hasType (argType sig) arg
-        then do
-          if hasType (valueType sig) val
-          then pure sig
-          else throwError $ InvalidValue (argType sig) arg
-        else throwError $ InvalidArgument (argType sig) arg
+    Nothing -> throwError $ UnknownCapability name
+    Just sig ->
+      if hasType (argType sig) arg
+      then
+        if hasType (valueType sig) val
+        then pure sig
+        else throwError $ InvalidValue (argType sig) arg
+      else throwError $ InvalidArgument (argType sig) arg
 
 -- Grant is also used to implement compose-capability
 grant :: forall s effs. MonadCapability s effs
@@ -116,31 +116,45 @@ grant name arg val = do
   sig <- lookupSignature @s @effs name arg val
   predicate sig arg val
 
-withCapability :: forall s effs r. MonadCapability s effs
-  => String -> PactValue -> PactValue -> Eff effs r -> Eff effs r
-withCapability name arg val action = do
-  caps <- grant name arg val
-  local @[[Capability s]] (caps :) action
-
-requireCapability :: forall s effs. MonadCapability s effs
-  => String -> PactValue -> PactValue -> Eff effs ()
-requireCapability name arg val = do
+checkCapability :: forall s effs. MonadCapability s effs
+  => String -> PactValue -> PactValue -> Eff effs (Maybe (Capability s))
+checkCapability name arg val = do
   _ <- lookupSignature @s @effs name arg val
   capss <- ask @[[Capability s]]
   go capss
  where
-  go [] = throwError $ CapabilityNotAvailable name arg
+  go [] = pure Nothing
   go (caps : capss) = go' caps capss
 
-  go' (Token cname carg : _) _       | name == cname && arg == carg = pure ()
-  go' (Managed cname carg ref : _) _ | name == cname && arg == carg = do
-    machine <- send $ readSTRef ref
-    case runMealyT machine val of
-        Left err -> throwError err
-        Right machine' -> send $ writeSTRef ref machine'
-
+  go' (tok@(Token cname carg) : _) _ | name == cname && arg == carg =
+    pure $ Just tok
+  go' (mng@(Managed cname carg _) : _) _ | name == cname && arg == carg = do
+    pure $ Just mng
   go' [] capss = go capss
   go' (_ : caps) capss = go' caps capss
+
+withCapability :: forall s effs r. MonadCapability s effs
+  => String -> PactValue -> PactValue -> Eff effs r -> Eff effs r
+withCapability name arg val action = do
+  mcap <- checkCapability @s @effs name arg val
+  case mcap of
+    Nothing -> do
+      caps <- grant name arg val
+      local @[[Capability s]] (caps :) action
+    Just _ -> action
+
+requireCapability :: forall s effs. MonadCapability s effs
+  => String -> PactValue -> PactValue -> Eff effs ()
+requireCapability name arg val = do
+  mcap <- checkCapability @s @effs name arg val
+  case mcap of
+    Just (Managed _ _ ref) -> do
+      machine <- send $ readSTRef ref
+      case runMealyT machine val of
+          Left err -> throwError err
+          Right machine' -> send $ writeSTRef ref machine'
+    Just _ -> pure ()
+    Nothing -> throwError $ CapabilityNotAvailable name arg
 
 {------------------------------------------------------------------------
  - Capability environment
